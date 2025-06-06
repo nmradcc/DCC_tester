7e0e9d5c9e55e39a3796b3abb1c8ab1fcf3a4aa9
David Vescovi <dvescovi@tampabay.rr.com>
2025-06-06 15:49:59 -0400

    able to read test.txt file!

--- Core/Src/fatfs.c
+++ Core/Src/fatfs.c
@@ -14,20 +14,53 @@
   *
   ******************************************************************************
   */
-
+#include <stdio.h>
 #include "fatfs.h"
+
+void Error_Handler(void);
 
 uint8_t retSD;    /* Return value for SD */
 char SDPath[4];   /* SD logical drive path */
 FATFS SDFatFS;    /* File system object for SD logical drive */
 FIL SDFile;       /* File object for SD */
 
+uint32_t file_error = 0, sd_detection_error = 0; 
+
 void FATFS_Init(void)
 {
   /*## FatFS: Link the SD driver ###########################*/
   retSD = FATFS_LinkDriver(&SD_Driver, SDPath);
 
+  if (retSD == 0)
+  {
+    /*## FatFS: Register the file system object to the FatFs module ############*/
+    if ((retSD = f_mount(&SDFatFS, (TCHAR const*)SDPath, 0)) != FR_OK)
+    {
+      /* FatFs mount Error */
+      printf("FatFs Driver f_mount failed with error code: %d\r\n", retSD);
+
+    }
+    else
+    {
+      /* FatFs Initialized successfully */
+      if((retSD = f_open(&SDFile, "test.txt", FA_READ)) == FR_OK)
+      {
+        printf("FatFs Driver f_open worked!!\r\n");
+        f_close(&SDFile);  
+      }
+      else /* Can't Open JPG file*/
+      {
+        printf("FatFs Driver f_open failed with error code: %d\r\n", retSD);
+      }       
+    }
+  }
+  else
+  {
+    /* Driver linking failed */
+    printf("FatFs Driver linking failed with error code: %d\r\n", retSD);
+  } 
 }
+
 
 /**
   * @brief  Gets Time from RTC
