dccaa4b573f1a5e5819dd173f1bbf9a391b71980
David Vescovi <david_vescovi@spectrum.net>
2025-06-13 12:07:19 -0400

    still working no filex

--- Core/Inc/tx_user.h
+++ Core/Inc/tx_user.h
@@ -76,7 +76,6 @@
 #define RTOS2_BYTE_POOL_HEAP_SIZE   (1024 * 16) /* 16 KB for ThreadX byte pool heap */
 #define RTOS2_BYTE_POOL_STACK_SIZE  (1024 * 64) /* 64 KB for ThreadX byte pool stack */
 
-#define TX_THREAD_USER_EXTENSION ULONG tx_thread_detached_joinable;
 /* USER CODE END 1 */
 
 /* Define various build options for the ThreadX port.  The application should either make changes
@@ -117,6 +116,7 @@
    to tx_port.h for descriptions on each of these options.  */
 
 #define TX_MAX_PRIORITIES                64
+#define TX_THREAD_USER_EXTENSION                ULONG tx_thread_detached_joinable;
 /*#define TX_TIMER_THREAD_STACK_SIZE                1024*/
 /*#define TX_TIMER_THREAD_PRIORITY                0*/
 
