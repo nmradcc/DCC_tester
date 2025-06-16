1193b2dee6f865963043688d3750837cf9ec4c09
David Vescovi <dvescovi@tampabay.rr.com>
2025-06-15 20:37:46 -0400

    w5

--- Core/Src/cli_app.c
+++ Core/Src/cli_app.c
@@ -10,22 +10,14 @@
 #include "main.h"
 #include "tx_api.h"
 #include "stm32h5xx_nucleo.h"
-#include "CLI.h"
+#include "cli_app.h"
 
 
 ULONG cli_queue_storage[5];
 
 TX_QUEUE cli_queue;
-TX_MUTEX cli_mutex;
-
-char OutputBuffer[MAX_OUTPUT_SIZE];
-char InputBuffer[MAX_INPUT_SIZE];
 
 int8_t cRxedChar;
-const char * cli_prompt = "\r\ncli> ";
-/* CLI escape sequences*/
-uint8_t backspace[] = "\b \b";
-uint8_t backspace_tt[] = " \b";
 
 void uart_receive_callback(char *input) {
     tx_queue_send(&cli_queue, input, TX_NO_WAIT);
@@ -45,19 +37,9 @@ int _write(int file, char *data, int len)
     return len;
 }
 
-
-/*************************************************************************************************/
-void cliWrite(const char *str)
-{
-   printf("%s", str);
-   // flush stdout
-   fflush(stdout);
-}
-/*************************************************************************************************/
 void vCommandConsoleTask(void *pvParameters)
 {
     (void)(pvParameters);
-    uint8_t cInputIndex = 0; // simply used to keep track of the index of the input string
     uint32_t receivedValue;  // used to store the received value from the notification
 
     tx_queue_create(&cli_queue, "Queue", TX_1_ULONG, cli_queue_storage, sizeof(cli_queue_storage));
