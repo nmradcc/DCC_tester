fd3d6d7f9578e47ba3182dc973e604fdbb23dadb
David Vescovi <dvescovi@tampabay.rr.com>
2025-06-15 21:18:14 -0400

    parsing

--- Core/Src/cli_app.c
+++ Core/Src/cli_app.c
@@ -7,22 +7,23 @@
 #include <assert.h>
 #include <string.h>
 
-#include "main.h"
 #include "tx_api.h"
 #include "stm32h5xx_nucleo.h"
 #include "cli_app.h"
 
 
-ULONG cli_queue_storage[5];
+ULONG command_queue_storage[5];
+TX_QUEUE command_queue;
 
-TX_QUEUE cli_queue;
-
-int8_t cRxedChar;
+static char InputBuffer[32];
+static char OutputBuffer[32];
+static unsigned int inputIndex = 0;
 
 void uart_receive_callback(char *input) {
-    tx_queue_send(&cli_queue, input, TX_NO_WAIT);
+    tx_queue_send(&command_queue, input, TX_NO_WAIT);
 }
 
+// Function to write data to UART
 int _write(int file, char *data, int len)
 {
     (void)(file);
@@ -40,17 +41,41 @@ int _write(int file, char *data, int len)
 void vCommandConsoleTask(void *pvParameters)
 {
     (void)(pvParameters);
-    uint32_t receivedValue;  // used to store the received value from the notification
-
-    tx_queue_create(&cli_queue, "Queue", TX_1_ULONG, cli_queue_storage, sizeof(cli_queue_storage));
+    uint32_t receivedChar;  // used to store the received value from the notification
+    char N_char = '\n';
+    tx_queue_create(&command_queue, "Queue", TX_1_ULONG, command_queue_storage, sizeof(command_queue_storage));
 
     for (;;)
     {
         // Wait for data from ISR
-        tx_queue_receive(&cli_queue, &receivedValue, TX_WAIT_FOREVER);
+        tx_queue_receive(&command_queue, &receivedChar, TX_WAIT_FOREVER);
+        if (receivedChar == '\b' || receivedChar == 0x7F) {
+            // user pressed backspace, remove last character from input string
+            if (inputIndex > 0) {
+                inputIndex--;
+                InputBuffer[inputIndex] = '\0'; // Null terminate the string
+                sprintf(OutputBuffer,"\b \b"); // Move cursor back, print space to overwrite, and move back again
+                _write(0, OutputBuffer, strlen(OutputBuffer)); // Echo backspace to console
+            }
+        }
+        else if (inputIndex < sizeof(InputBuffer) - 1) {
+            // user pressed a character add it to the input string
+            InputBuffer[inputIndex++] = receivedChar;
+            InputBuffer[inputIndex] = '\0'; // Null terminate the string
+            _write(0, (char *)&receivedChar, 1); // Echo the character to the console
+            if (receivedChar == '\r' || receivedChar == '\n') {
+                // Process the command when Enter is pressed
+                InputBuffer[inputIndex - 1] = '\0'; // Null terminate the string
+                inputIndex = 0; // Reset input index for next command
+                _write(0, &N_char, 1); // Echo the character to the console
+                // Here you can add code to parse and execute the command
+            }   
+        }
+        else if (inputIndex >= sizeof(InputBuffer) - 1) {
+            // input buffer is full, ignore further input
+        }
 
-        //echo recevied char
-        cRxedChar = receivedValue & 0xFF;
+
     }
 }
 
