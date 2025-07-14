1cf3c4c252e0c0e8e731e54b0bc445e532217785
David Vescovi <david_vescovi@spectrum.net>
2025-07-14 08:59:45 -0400

    cs tweaks

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -114,9 +114,6 @@ int main(void)
 
   /* USER CODE BEGIN 1 */
 
-  // Override alignment issue with DCC library on M33
-  SCB->CCR &= ~SCB_CCR_UNALIGN_TRP_Msk;
-
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -125,6 +122,9 @@ int main(void)
   HAL_Init();
 
   /* USER CODE BEGIN Init */
+
+  // Override alignment issue with DCC library on M33
+  SCB->CCR &= ~SCB_CCR_UNALIGN_TRP_Msk;
 
   /* USER CODE END Init */
 
