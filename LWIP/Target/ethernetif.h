1204b92fc976866549fac40a1380f1903a381082
David Vescovi <dvescovi@tampabay.rr.com>
2025-08-11 11:06:52 -0400

    attempt

--- LWIP/Target/ethernetif.h
+++ LWIP/Target/ethernetif.h
@@ -1,13 +1,12 @@
-/* USER CODE BEGIN Header */
 /**
- ******************************************************************************
-  * File Name          : ethernetif.h
-  * Description        : This file provides initialization code for LWIP
-  *                      middleWare.
+  ******************************************************************************
+  * @file    LwIP/LwIP_TCP_Echo_Client/LWIP/Target/ethernetif.h
+  * @author  MCD Application Team
+  * @brief   Header for ethernetif.c module
   ******************************************************************************
   * @attention
   *
-  * Copyright (c) 2023 STMicroelectronics.
+  * Copyright (c) 2024 STMicroelectronics.
   * All rights reserved.
   *
   * This software is licensed under terms that can be found in the LICENSE file
@@ -16,31 +15,16 @@
   *
   ******************************************************************************
   */
-/* USER CODE END Header */
 
 #ifndef __ETHERNETIF_H__
 #define __ETHERNETIF_H__
 
+
 #include "lwip/err.h"
 #include "lwip/netif.h"
-#include "cmsis_os.h"
 
-/* Within 'USER CODE' section, code will be kept by default at each generation */
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/* Exported functions ------------------------------------------------------- */
+/* Exported types ------------------------------------------------------------*/
 err_t ethernetif_init(struct netif *netif);
-
-void ethernetif_input(void* argument);
-void ethernet_link_thread(void* argument);
-
-void Error_Handler(void);
-u32_t sys_jiffies(void);
-u32_t sys_now(void);
-
-/* USER CODE BEGIN 1 */
-
-/* USER CODE END 1 */
+void ethernetif_input(struct netif *netif);
+void ethernet_link_check_state(struct netif *netif);
 #endif
